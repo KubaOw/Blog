1a4fad8813aed7d6bd152c67cb4a22f95cd3ef34
KubaOw <kuba.owczarek@icloud.com>
2023-03-28 17:11:12 +0200

    Configuration
    Created context for database , configuration like connectionString, added first four classes that represents future database tables

---
+++ Blog/Blog/Entities/Context.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Blog.Entities
+{
+    public class Context : DbContext
+    {
+        public Context(DbContextOptions<Context> options) : base(options)
+        { 
+
+        }
+        public DbSet<Answer> Answers { get;set; }
+        public DbSet<Question> Questions { get;set; }
+        public DbSet<Tag> Tags { get;set; }
+        public DbSet<User> Users { get; set; } 
+    }
+}
+    
---
+++ Blog/Blog/Entities/Question.cs
@@ -0,0 +1,9 @@
+﻿namespace Blog.Entities
+{
+    public class Question
+    {
+        public Guid Id { get; set; }
+        public string Value { get; set; }
+        public string Title { get; set; }
+    }
+}
